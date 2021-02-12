87522ccb9501979c264e91b8d22528b934c88183
F. Anselmo <falc@null.net>
2021-02-12 21:12:55 +0100

    EditableNodeSelector

--- src/components/EditorModal/EditorModal.jsx
+++ src/components/EditorModal/EditorModal.jsx
@@ -1,64 +1,68 @@
 import React, { useState, useEffect } from "react";
-import styled from 'styled-components';
-import classnames from 'classnames';
-import { useRecoilState, useRecoilValue } from 'recoil';
+import styled from "styled-components";
+import classnames from "classnames";
+import { useRecoilState, useRecoilValue } from "recoil";
 
 import { editorVisibilityState, editableNodeState } from "stores/editor";
-import { treeNodesState } from "stores/file";
 
-import useEditorModal from './useEditorModal.js';
-import Editor from "components/QuillEditor/QuillEditor.jsx"
+import useEditorModal from "./useEditorModal.js";
+import Editor from "components/QuillEditor/QuillEditor.jsx";
 
 const EditorModal = ({ className }) => {
-    const [ isEditorVisible, setEditorVisibility ] = useRecoilState(editorVisibilityState);
-    const nodeId = useRecoilValue(editableNodeState);
-    const treeNodes = useRecoilValue(treeNodesState);
-    const [internalValue, setInternalValue] = useState("");
-    const { changeValue } = useEditorModal();
-  
-    useEffect(() => {
-        if (treeNodes && nodeId && treeNodes[nodeId]) {
-            setInternalValue(treeNodes[nodeId].text)
-        }
-    }, [treeNodes, nodeId])
+  const [isEditorVisible, setEditorVisibility] = useRecoilState(
+    editorVisibilityState
+  );
+  const node = useRecoilValue(editableNodeState);
+  const [internalValue, setInternalValue] = useState("");
+  const { changeValue } = useEditorModal();
 
-    if (!isEditorVisible) return null;
+  useEffect(() => {
+    if (node) {
+      setInternalValue(node.text);
+    }
+  }, [node]);
 
-    return (
-        <div className={classnames(className, "modal")}>
-            <button 
-                className="modal__close-button"
-                onClick={() => { setEditorVisibility(false) }}
-            >
-                Close
-            </button>
-            <Editor
-                className="modal__editor"
-                onChange={html => setInternalValue(html)}
-                onSave={() => { changeValue(nodeId, internalValue); }}
-                value={internalValue}
-            />
-        </div>
-    );
+  if (!isEditorVisible) return null;
+
+  return (
+    <div className={classnames(className, "modal")}>
+      <button
+        className="modal__close-button"
+        onClick={() => {
+          setEditorVisibility(false);
+        }}
+      >
+        Close
+      </button>
+      <Editor
+        className="modal__editor"
+        onChange={(html) => setInternalValue(html)}
+        onSave={() => {
+          changeValue(node.id, internalValue);
+        }}
+        value={internalValue}
+      />
+    </div>
+  );
 };
 
 export default styled(EditorModal)`
-background-color: white;
-display: flex;
-flex-direction: column;
-z-index: 5;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  z-index: 5;
 
-.modal__close-button {
+  .modal__close-button {
     align-self: flex-start;
-}
+  }
 
-@media screen and (max-width: 500px) {
+  @media screen and (max-width: 500px) {
     & {
-        position: absolute;
-        top: 0px;
-        left: 0px;
-        width: 100vw;
-        height: 100vh;
+      position: absolute;
+      top: 0px;
+      left: 0px;
+      width: 100vw;
+      height: 100vh;
     }
-}
+  }
 `;
--- src/stores/editor.js
+++ src/stores/editor.js
@@ -1,11 +1,26 @@
-import { atom } from 'recoil';
+import { atom, selector } from "recoil";
+import { treeNodeFamily } from "./file";
 
 export const editorVisibilityState = atom({
-    key: 'editorVisibility',
-    default: false
+  key: "editorVisibility",
+  default: false
 });
 
-export const editableNodeState = atom({
-    key: 'editableNode',
-    default: undefined
+export const editableNodeIdState = atom({
+  key: "editableNodeId",
+  default: undefined
+});
+
+export const editableNodeState = selector({
+  key: "editableNode",
+  get: ({ get }) => {
+    const id = get(editableNodeIdState);
+    if (id) {
+      return get(treeNodeFamily(id));
+    }
+    return null;
+  },
+  set: ({ set }, newValue) => {
+    set(editableNodeIdState, newValue);
+  }
 });
